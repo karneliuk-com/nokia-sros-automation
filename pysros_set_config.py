78cb8e862750ef264be7e5754cb69ca9aec1b7c8
akarneliuk <a.karneliuk@gmail.com>
2021-10-30 20:53:00 +0000

    0.3.1

--- pysros_set_config.py
+++ pysros_set_config.py
@@ -5,6 +5,67 @@ import datetime
 import os
 from dotenv import load_dotenv
 from pysros.management import connect
+from pysros.pprint import printTree
+
+
+# Variables
+xpath = "/openconfig-interfaces:interfaces/interface[name=\"loopback\"]"
+yang_container_value = {
+                            "config": {
+                                "name": "loopback",
+                                "type": "softwareLoopback",
+                                "description": "test-pysros-loopback",
+                                "enabled": True
+                            },
+                            "subinterfaces": {
+                                "subinterface": {
+                                    0: {
+                                        "config": {
+                                            "index": 0
+                                        },
+                                        "ipv4": {
+                                            "addresses": {
+                                                "address": {
+                                                    "10.0.254.11": {
+                                                        "config": {
+                                                            "ip": "10.0.254.11",
+                                                            "prefix-length": 32
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        },
+                                        "ipv6": {
+                                            "addresses": {
+                                                "address": {
+                                                    "fc00:10:0:254::11": {
+                                                        "config": {
+                                                            "ip": "fc00:10:0:254::11",
+                                                            "prefix-length": 128
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                       }
+
+xpath2 = "/openconfig-interfaces:interfaces/interface[name=\"loopback\"]/config/description"
+description_str = "new-pysros-description"
+
+# Functions 
+def get_nokia_netconf(nc_sesion, xpath: str) -> dict:
+    result = {}
+
+    try:
+        result = nc_sesion.running.get(path=xpath)
+
+    except LookupError as e:
+        result = f"{e} for {xpath}"
+
+    return result
 
 
 # Body
@@ -17,9 +78,6 @@ if __name__ == "__main__":
             "password": os.getenv("NOKIA_PASS")
          }
 
-    ## Get path
-    path = '/nokia-conf:configure/card[slot-number="1"]'
-
     ## Timestamp: started
     t1 = datetime.datetime.now()
 
@@ -27,15 +85,17 @@ if __name__ == "__main__":
     connect_obj = connect(host=host["ip_address"], username=host["username"],
                           password=host["password"])
 
-    r1 = connect_obj.running.get(path="/openconfig-interfaces:interfaces/interface[name=\"loopback\"]")
-
-    # Setting a value (YANG leaf or list-leaf type )
-    r2 = connect_obj.candidate.set(path="/openconfig-interfaces:interfaces/interface[name=\"loopback\"]/config/description", value="test configuration")
+    ## Collect the configuration of an interface in OpenConfig YANG modules
+    r1 = get_nokia_netconf(nc_sesion=connect_obj, xpath=xpath)
+#    r1 = get_nokia_netconf(nc_sesion=connect_obj, xpath=xpath2)
 
     # Setting a nested value (YANG container or list)
-    r3 = connect_obj.candidate.set(path="/openconfig-interfaces:interfaces/interface[name=\"loopback\"]/subinterfaces/subinterface", value={1: {"config": {"index": 1, "description": "another test", "enabled": True}}})
+    connect_obj.candidate.set(path=xpath, value=yang_container_value)
+#    connect_obj.candidate.set(path=xpath2, value=description_str)
 
-    r4 = connect_obj.running.get(path="/openconfig-interfaces:interfaces/interface[name=\"loopback\"]")
+    ## Collect the configuration of an interface in OpenConfig YANG modules
+    r2 = get_nokia_netconf(nc_sesion=connect_obj, xpath=xpath)
+#    r2 = get_nokia_netconf(nc_sesion=connect_obj, xpath=xpath2)
 
     connect_obj.disconnect()
 
@@ -47,9 +107,13 @@ if __name__ == "__main__":
     print(f"{'=' * tl.columns}\nCompleted in {t2 -t1}\n{'=' * tl.columns}\n")
 
     ## Printing collected data
-    print(r1)
-    print(r2)
-    print(r3)
-    print(r4)
+    print("Pre-change collection:")
+    if isinstance(r1, str):
+        print(r1)
+    else:
+        printTree(r1)
+
+    print("\nPost-change collection:")
+    printTree(r2)
 
     print(f"\n{'=' * tl.columns}\n")
