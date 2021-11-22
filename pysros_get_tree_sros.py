356d2f230e04c091b0c1bc7f6f02110b749b84ee
akarneliuk <a.karneliuk@gmail.com>
2021-11-22 23:36:43 +0000

    0.4.1

--- pysros_get_tree_sros.py
+++ pysros_get_tree_sros.py
@@ -20,13 +20,12 @@ if __name__ == "__main__":
         t1 = time.ticks_ms()
 
         ## Collect the information
-        full_xpath_str = "/" + path_str
         connect_obj = connect()             
-        results = connect_obj.running.get(full_xpath_str)
+        results = connect_obj.running.get(path_str)
         connect_obj.disconnect()
 
         ## Print results
-        print("Reuqested path: \n{}\n\nResult:".format(full_xpath_str))   
+        print("Reuqested path: \n{}\n\nResult:".format(path_str))   
         printTree(results)
 
         ## Get the timesamp at the end
